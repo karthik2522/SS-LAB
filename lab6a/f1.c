b4155780fc74627ecf866c032036fa0ff7679bef
KHP <61647777+karthik2522@users.noreply.github.com>
2021-05-30 14:30:48 +0530

    Create f1.c

---
+++ lab6a/f1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+int a,b,c;
+
+/* Declaration */
+printf("-----------------------------------------");
+scanf("---------------------");
+
+//for reading 
+
+getch();
+}
